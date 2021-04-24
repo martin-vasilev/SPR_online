deaa7d25a9ea682dabd5580015253c027a9974b3
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2021-04-24 14:11:06 +0100

    Prolific data

---
+++ data/Prolific_preproc.R
@@ -0,0 +1,126 @@
+
+rm(list= ls())
+
+
+get_num<- function(string){as.numeric(unlist(gsub("[^0-9]", "", unlist(string)), ""))}
+source("https://raw.githubusercontent.com/martin-vasilev/R_scripts/master/LabJs_device.R")
+
+library(readr)
+
+files<- list.files("data/Prolific/") # get all available files in directory
+files<-paste("data/Prolific/", files, sep= '') # paste full root link
+
+dat<- NULL
+
+for(i in 1:length(files)){ # for each participant file..
+  t<- suppressWarnings(suppressMessages(read.csv(files[i]))) # load it up
+  t$subject<- i # assign subject number
+
+  # get experiment duration:
+  start<- substr(t$timestamp[1], 12, 19)
+  end<-  substr(t$timestamp[nrow(t)], 12, 19)
+  duration<- sprintf("%g", strptime(end, "%H:%M:%S")- strptime(start, "%H:%M:%S"))
+  
+  cat(sprintf("sub %g: %g columns\n", i, nrow(t))) # output rows for monitoring
+  cat(sprintf("Experiment time: %s (min)\n\n", duration))
+  t$exp_time<- as.numeric(duration)
+  
+  ## trap accuracy:
+  trap<- subset(t, sender== "trap_trial")
+  
+  trap$accuracy<- ifelse(trap$correct== "true", 1, 0)
+  t$trap_accuracy<- sum(trap$accuracy)/4
+  
+  
+  q1<- subset(t, sender== "Question 1")
+  t$which_list= q1$list[1]
+  
+  dat<- plyr::rbind.fill(dat, t) # combine with available dataset
+  
+}
+
+
+### DEMOGRAPHIC DATA:
+d<- subset(dat, sender== "Demography form") # extract demographic data
+honesty<- subset(dat, sender== "wore_headphones")
+dem<- data.frame("subject" = d$subject, "gender"= d$gender, "age"= d$age, 
+                 "education"= d$education,
+                 "list"= d$which_list, 
+                 "experiment_time"= d$exp_time,
+                 "trap_accuracy"= d$trap_accuracy,
+                 "honesty"= honesty$response) # only info we need
+#write.csv(dem, "data/participant_data.csv", row.names = F) # save device info
+
+
+### DEVICE DATA:
+device<- LabJs_device(dat) # extract devide info from lab.js
+#write.csv(device, "data/device_info.csv", row.names = F) # save device info
+
+
+### QUESTION ACCURACY DATA:
+dat$accuracy<- ifelse(dat$correct== "true", 1, 0) # convert accuracy to binomial data:
+
+q<- subset(dat, is.element(sender, c("Question 1","Question 2"))) # extract just questions
+q<- q[, c("subject","item", "Provo_ID", "list", "accuracy", "duration",   # save just columns we need
+          "sound", "ended_on", "response", "correctResponse")]
+#q<- subset(q, item<20) # remove practice
+
+q$item_quest<- rep(c(1,2), nrow(q)/2)
+
+#write.csv(q, "data/question_accuracy.csv", row.names = F) # save accuracy data
+
+
+### REACTION TIME DATA:
+rt<- subset(dat, sender== "screen") # subset reaction time data
+rt<- rt[, c("subject", "item", "Provo_ID", "list", "word", "word_ID",  # save just columns we need
+            "ended_on", "duration", "sound")]
+rt<- subset(rt, item<20) # remove practice items
+
+#rt<- subset(rt, ended_on== "response")
+
+#write.csv(rt, "data/reaction_time.csv", row.names = F) # save accuracy data
+
+
+### MUSIC RATING DATA:
+ratings<- subset(dat, sender == "song_ratings")
+
+ratings<- ratings[, c("subject", "which_list", "song_rating", "snippet_file", "familiarity",
+                      "preference", "pleasantness", "offensiveness", 
+                      "distraction", "artist_name", "song_name" )]
+write.csv(ratings, "data/music_ratings.csv", row.names = F) # save accuracy data
+
+
+### Music preferences:
+
+genres<- subset(dat, sender== "Music styles")
+genres<- genres[, c("subject", "Blues",  "Classical", "Country", "Dance", "Electronic", 
+                    "Folk", "Gospel", "Hip.Hop", "Jazz", "Latin",
+                    "Musical.Film", "New.age", "Pop", "R.B",
+                    "Rap", "Reggae", "Religious", "Rock",  "Soul",
+                    "Swing", "Traditional")] # subset colums we need
+
+
+freq<- subset(dat, sender== "Music_frequency")
+freq<- freq[, c("subject", "music_frequency")]
+
+preference <- merge(freq, genres) # merge two dataframes
+#write.csv(preference, "data/music_preferences.csv") # save data
+
+### TRIAL DURATIONS
+
+trial_time<- subset(dat, is.element(sender, c(paste("Item ", 1:12, sep= ''))))
+
+trial_time<- trial_time[, c("subject", "sender", "duration"),]
+
+
+table(dem$list)
+table(q$item, q$sound)
+table(rt$item, rt$sound)
+
+#rt<- rt[which(rt$duration>100 & rt$duration<5000), ]
+
+
+# hist(ratings$familiarity, breaks= 10)
+# hist(ratings$preference, breaks= 10)
+
+table(dem$list)
