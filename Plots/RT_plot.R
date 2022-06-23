fb7cf9c3edc6c1f7ce4609ce3f0bde67447101e3
Martin Vasilev <mvasilev@bournemouth.ac.uk>
2022-06-23 15:12:35 +0100

    All exp plots

---
+++ Plots/RT_plot.R
@@ -0,0 +1,103 @@
+
+rm(list = ls())
+
+# colorblind palletes: # https://venngage.com/blog/color-blind-friendly-palette/
+pallete1= c("#CA3542", "#27647B", "#849FA0", "#AECBC9", "#57575F") # "Classic & trustworthy"
+
+library(reshape)
+library(ggplot2)
+
+# Experiment 1a:
+rt_e1 <- read_csv("Experiment1a/data/reaction_time.csv")
+
+DesRT_e1<- melt(rt_e1, id=c('subject', 'item', 'sound'), 
+             measure=c("duration"), na.rm=TRUE)
+
+mRT_e1<- cast(DesRT_e1, sound+subject ~ variable
+           ,function(x) c(M=signif(mean(x),3)
+                          , SD= sd(x) ))
+mRT_e1$Experiment<- "Experiment 1a"
+
+
+# Experiment 1b:
+rt_e2 <- read_csv("Experiment1b/data/reaction_time.csv")
+
+DesRT_e2<- melt(rt_e2, id=c('subject', 'item', 'sound'), 
+                measure=c("duration"), na.rm=TRUE)
+
+mRT_e2<- cast(DesRT_e2, sound+subject ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+mRT_e2$Experiment<- "Experiment 1b"
+
+
+
+# Experiment 2:
+rt_e3 <- read_csv("Experiment2/data/reaction_time.csv")
+
+DesRT_e3<- melt(rt_e3, id=c('subject', 'item', 'sound'), 
+                measure=c("duration"), na.rm=TRUE)
+
+mRT_e3<- cast(DesRT_e3, sound+subject ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+mRT_e3$Experiment<- "Experiment 2"
+
+
+# Experiment 3:
+rt_e4 <- read_csv("Experiment3/data/reaction_time.csv")
+
+DesRT_e4<- melt(rt_e4, id=c('subject', 'item', 'sound'), 
+                measure=c("duration"), na.rm=TRUE)
+
+mRT_e4<- cast(DesRT_e4, sound+subject ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+mRT_e4$Experiment<- "Experiment 3"
+
+mRT<- rbind(mRT_e1, mRT_e2, mRT_e3, mRT_e4)
+
+mRT$sound<- as.factor(mRT$sound)
+mRT$sound<- factor(mRT$sound, levels = c("silence", "instrumental", "lyrical", "speech"))
+#levels(mRT$sound)<- c("silence","instrumental music", "lyrical music"  )
+
+levels(mRT$sound)
+
+
+fun_mean <- function(x){
+  return(data.frame(y=mean(x),label= paste("M= ", round(mean(x,na.rm=T)), sep= '')))}
+
+MPlot <-ggplot(mRT, aes(x = sound, y = duration_M, color= sound, fill= sound)) + 
+  ggdist::stat_halfeye(
+    adjust = .7, 
+    width = .6, 
+    .width = 0, 
+    justification = -.3, 
+    point_colour = NA) + 
+  geom_boxplot(
+    width = .25, 
+    outlier.shape = NA, fill= NA
+  ) +
+  geom_point(
+    size = 1.,
+    alpha = .3,
+    position = position_jitter(
+      seed = 1, width = .13
+    )
+  ) + 
+  coord_cartesian(xlim = c(1.2, NA), clip = "off")+
+  scale_color_manual(values=pallete1)+
+  scale_fill_manual(values=pallete1)+
+  theme_classic(20) +ylab("Mean reaction time per word (in ms)")+
+  theme(legend.position = 'none')+
+  stat_summary(fun = mean, geom="point",colour="black", size=2) +
+  stat_summary(fun.data = fun_mean, geom="text", vjust=-0.7, colour="black", size= 4)+
+  facet_wrap(~Experiment)
+
+MPlot
+
+
+ggsave(plot = MPlot, filename = "Plots/RT_mean.pdf", height = 11, width = 11)
+
+
+
---
+++ Plots/music_ratings.R
@@ -0,0 +1,144 @@
+
+rm(list = ls())
+
+library(reshape)
+library(ggplot2)
+
+# colorblind palletes: # https://venngage.com/blog/color-blind-friendly-palette/
+pallete1= c("#CA3542", "#27647B", "#849FA0", "#AECBC9", "#57575F") # "Classic & trustworthy"
+
+fun_mean <- function(x){
+  return(data.frame(y=mean(x),label= paste("M= ", round(mean(x,na.rm=T),1), sep= '')))}
+
+
+######## Experiment 1a:
+
+ratings1 <- read.csv("Experiment1a/data/prep/ratings_manual_coding.csv", sep=";")
+
+DesSongs1<- melt(ratings1, id=c('subject', 'music', 'song_number', 'music_set'), 
+                measure=c("familiarity", 'preference', 'pleasantness',
+                          'offensiveness', 'distraction', 
+                          'accuracy_artist', 'accuracy_song'), na.rm=TRUE)
+mSongs1<- cast(DesSongs1, music+song_number+music_set ~ variable
+              ,function(x) c(M=signif(mean(x),3)
+                             , SD= sd(x) ))
+
+df1<-  data.frame("Mean"= c(mSongs1$familiarity_M, mSongs1$preference_M, mSongs1$pleasantness_M, 
+                           mSongs1$offensiveness_M, mSongs1$distraction_M),
+                 "SD"= c(mSongs1$familiarity_SD, mSongs1$preference_SD, mSongs1$pleasantness_SD, 
+                         mSongs1$offensiveness_SD, mSongs1$distraction_SD),
+                 "Measure"= c(rep("Familiarity", 12), rep("Preference", 12),
+                              rep("Pleasantness", 12), rep("Offensiveness", 12),
+                              rep("Distraction",12)),
+                 "Music"= rep(mSongs1$music, 5))#,
+#"Music"= rep(c("Instrumental", "Lyrical"), 5*204))
+df1$Experiment<- "Experiment 1a"
+
+
+
+######## Experiment 1b:
+
+ratings2 <- read.csv("Experiment1b/data/prep/ratings_manual_coding.csv", sep=";")
+
+DesSongs2<- melt(ratings2, id=c('subject', 'music', 'song_number', 'music_set'), 
+                 measure=c("familiarity", 'preference', 'pleasantness',
+                           'offensiveness', 'distraction', 
+                           'accuracy_artist', 'accuracy_song'), na.rm=TRUE)
+mSongs2<- cast(DesSongs2, music+song_number+music_set ~ variable
+               ,function(x) c(M=signif(mean(x),3)
+                              , SD= sd(x) ))
+
+df2<-  data.frame("Mean"= c(mSongs2$familiarity_M, mSongs2$preference_M, mSongs2$pleasantness_M, 
+                            mSongs2$offensiveness_M, mSongs2$distraction_M),
+                  "SD"= c(mSongs2$familiarity_SD, mSongs2$preference_SD, mSongs2$pleasantness_SD, 
+                          mSongs2$offensiveness_SD, mSongs2$distraction_SD),
+                  "Measure"= c(rep("Familiarity", 12), rep("Preference", 12),
+                               rep("Pleasantness", 12), rep("Offensiveness", 12),
+                               rep("Distraction",12)),
+                  "Music"= rep(mSongs2$music, 5))#,
+
+df2$Experiment<- "Experiment 1b"
+
+
+
+
+
+######## Experiment 2:
+
+ratings3 <- read.csv("Experiment2/data/prep/ratings_manual_coding.csv", sep=",")
+
+DesSongs3<- melt(ratings3, id=c('subject', 'music', 'song_number', 'music_set'), 
+                 measure=c("familiarity", 'preference', 'pleasantness',
+                           'offensiveness', 'distraction', 
+                           'accuracy_artist', 'accuracy_song'), na.rm=TRUE)
+mSongs3<- cast(DesSongs3, music+song_number+music_set ~ variable
+               ,function(x) c(M=signif(mean(x),3)
+                              , SD= sd(x) ))
+
+df3<-  data.frame("Mean"= c(mSongs3$familiarity_M, mSongs3$preference_M, mSongs3$pleasantness_M, 
+                            mSongs3$offensiveness_M, mSongs3$distraction_M),
+                  "SD"= c(mSongs3$familiarity_SD, mSongs3$preference_SD, mSongs3$pleasantness_SD, 
+                          mSongs3$offensiveness_SD, mSongs3$distraction_SD),
+                  "Measure"= c(rep("Familiarity", 12), rep("Preference", 12),
+                               rep("Pleasantness", 12), rep("Offensiveness", 12),
+                               rep("Distraction",12)),
+                  "Music"= rep(mSongs3$music, 5))#,
+
+df3$Experiment<- "Experiment 2"
+
+
+
+
+######## Experiment 3:
+
+ratings4 <- read.csv("Experiment3/data/prep/ratings_manual_coding.csv", sep=";")
+
+DesSongs4<- melt(ratings4, id=c('subject', 'music', 'song_number', 'music_set'), 
+                 measure=c("familiarity", 'preference', 'pleasantness',
+                           'offensiveness', 'distraction', 
+                           'accuracy_artist', 'accuracy_song'), na.rm=TRUE)
+mSongs4<- cast(DesSongs4, music+song_number+music_set ~ variable
+               ,function(x) c(M=signif(mean(x),3)
+                              , SD= sd(x) ))
+
+df4<-  data.frame("Mean"= c(mSongs4$familiarity_M, mSongs4$preference_M, mSongs4$pleasantness_M, 
+                            mSongs4$offensiveness_M, mSongs4$distraction_M),
+                  "SD"= c(mSongs4$familiarity_SD, mSongs4$preference_SD, mSongs4$pleasantness_SD, 
+                          mSongs4$offensiveness_SD, mSongs4$distraction_SD),
+                  "Measure"= c(rep("Familiarity", 12), rep("Preference", 12),
+                               rep("Pleasantness", 12), rep("Offensiveness", 12),
+                               rep("Distraction",12)),
+                  "Music"= rep(mSongs4$music, 5))#,
+
+df4$Experiment<- "Experiment 3"
+
+
+df<-rbind(df1, df2, df3, df4)
+df$Measure<- as.factor(df$Measure)
+df$Measure<- factor(df$Measure, levels= c("Familiarity", "Preference", "Pleasantness", "Distraction", "Offensiveness"))
+
+
+
+
+a <- ggplot(df, aes(x = Measure, y= Mean, fill= Music, color= Music))+
+  geom_boxplot(
+    width = .25, 
+    outlier.shape = NA, fill= NA, position = position_dodge(width = 0.8)
+  ) +
+  geom_point(aes(fill=Music),
+             size = 1.3,
+             alpha = .6,
+             position =  position_jitterdodge(jitter.width = 0.1, dodge.width = 0.8)
+  ) + 
+  coord_cartesian(xlim = c(1.2, NA), clip = "off")+
+  scale_color_manual(values=pallete1[1:2])+
+  scale_fill_manual(values=pallete1[1:2])+
+  theme_classic(20) +ylab("Mean rating (1= very low; 10= very high)")+
+  theme(legend.position = 'top')+
+  stat_summary(fun = mean, geom="point",colour="black", size=3, position = position_dodge(0.8), show.legend = F) +
+  stat_summary(fun.data = fun_mean, geom="text", vjust=-0.8, colour="black", position = position_dodge(0.8))+
+  facet_wrap(~Experiment)
+
+ggsave(plot = a, filename = "Plots/Ratings_M.pdf", height = 10, width = 15)
+
+
