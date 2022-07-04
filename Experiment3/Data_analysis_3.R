7ef9467e0238f61b578a5753a30eb6004a52d352
Martin R. Vasilev <martin.r.vasilev@gmail.com>
2022-07-04 17:44:01 +0100

    models

--- Experiment3/Data_analysis_3.R
+++ Experiment3/Data_analysis_3.R
@@ -90,6 +90,50 @@ effect('sound', LM1)
 
 
 
+#### Bayesian model parameters:
+NwarmUp<- 500#500
+Niter<- 5000#2500
+Nchains<- 4 #10
+
+
+job::job({
+  
+  BM1<- brm(formula = log_duration ~ sound + (1|subject)+ (1|item), data = rt, 
+            warmup = NwarmUp, iter = Niter, chains = Nchains,
+            sample_prior = TRUE, cores = detectCores(), seed= 1234, control = list(adapt_delta = 0.9),
+            prior =  c(set_prior('normal(0, 0.05)', class = 'b', coef= 'sound.instr_vs_slc'),
+                       set_prior('normal(0, 0.05)', class = 'b', coef= 'sound.lyr_vs_instr'),
+                       set_prior('normal(0, 0.75)', class = 'b', coef= 'soundspeech_vs_instr'),
+                       set_prior('normal(0, 6)', class = 'Intercept')))
+}) 
+
+
+A= print(BM1, digits=5)
+prior_summary(BM1)
+
+save(BM1, file= "Experiment3/models/BM1.Rda")
+
+## Bayes factors:
+
+# Note: the Bayes Factor is BH_10, so values >1 indicate evidence for the alternative, and values <1 indicate 
+# evidence in support of the null. Brms reports them the other way around, but I reverse them here because I 
+# Think BF_10 reporting is somewhat more common
+
+# sound effect 1:
+BF_sound1 = hypothesis(BM1, hypothesis = 'sound.instr_vs_slc = 0', seed= 1234)  # H0: No  slc vs instr difference
+(BF1= 1/BF_sound1$hypothesis$Evid.Ratio)
+
+# sound effect 2:
+BF_sound2 = hypothesis(BM1, hypothesis = 'sound.lyr_vs_instr = 0', seed= 1234)  # H0: No  lyr vs instr difference
+(BF2= 1/BF_sound2$hypothesis$Evid.Ratio)
+
+# sound effect 3:
+BF_sound3 = hypothesis(BM1, hypothesis = 'soundspeech_vs_instr = 0', seed= 1234)  # H0: No  lyr vs instr difference
+(BF3= 1/BF_sound3$hypothesis$Evid.Ratio)
+
+
+
+
 
 ## Main model with accuracy data:
 if(!file.exists("Experiment3/models/LM2.Rda")){
@@ -117,6 +161,7 @@ job::job({
                        set_prior('normal(0, 0.75)', class = 'b', coef= 'soundspeech_vs_instr'),
                        set_prior('normal(0, 2)', class = 'Intercept')))
 }) 
+
 
 A= print(GM1, digits=3)
 save(GM1, file= "Experiment3/models/GM1.Rda")
